72730f94feb889caf19f9899d378bd672a4cfb25
George0717 <raden7george@gmail.com>
2023-03-15 22:41:51 +0700

    update

--- p6/latihan/dashboard.php
+++ p6/latihan/dashboard.php
@@ -38,7 +38,7 @@ if (isset($_POST['username']) && isset($_POST['nama'])) {
   <!-- Page Content -->
   <section class="py-5 mt-5 " style="background-image: linear-gradient(to right top, #051937, #0b2d5a, #104480, #125ba9, #0d74d3); color: white">
     <div class="container text-center">
-      <h2 class="fw-light">Selamat Datang
+      <h2 class="fw-light" style="margin-top:20px">Selamat Datang
         <br>
         <?php
         if ($_SESSION['username'] && $_SESSION['nama']) {
@@ -50,11 +50,11 @@ if (isset($_POST['username']) && isset($_POST['nama'])) {
     </div>
   </section>
   <section class="section2">
-    <h3 class="text-center mt-3">Apakah yang sedang anda cari?</h3>
+    <h3 class="text-center mt-3">Kursus</h3>
     <hr>
     <table>
       <td>
-        <div class="card" style="width: 18rem; margin-left: 30px">
+        <div class="card card1" style="width: 18rem; margin-left: 200px">
           <img src="img/ecology.png" class="card-img-top" alt="..." ">
           <div class="card-body">
             <p class="card-text text-center">Komputer Hijau</p>
@@ -62,16 +62,24 @@ if (isset($_POST['username']) && isset($_POST['nama'])) {
         </div>
       </td>
       <td>
-        <div class="card card2" style="width: 18rem; margin-left: 20px">
-          <img src="img/ecology.png" class="card-img-top" alt="..." >
+        <div class="card " style="width: 18rem;">
+          <img src="img/ml.png" class="card-img-top" alt="..." >
           <div class="card-body">
-            <p class="card-text text-center">Komputer Hijau</p>
+            <p class="card-text text-center">Machine Learning</p>
+          </div>
+        </div>
+      </td>
+      <td>
+        <div class="card " style="width: 18rem; ">
+          <img src="img/ba.png" class="card-img-top" alt="..." >
+          <div class="card-body">
+            <p class="card-text text-center">Business Analyst</p>
           </div>
         </div>
       </td>
 
     </table>
   </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- p6/latihan/logout.php
+++ p6/latihan/logout.php
@@ -26,13 +26,10 @@
         echo $_SESSION['username'];
         echo $_SESSION['name'];
     }
-    else {
-        echo "Kosong";
-    }
     ?>
     <h2>Apakah yakin mau log out???</h2>
  <form action="" method="POST">
-    <input type="submit" value="unset session" name="out">
+ <button class="btn" value="unset session" name="out">Hover Me!</button>
     </form>
  </body>
  </html>
